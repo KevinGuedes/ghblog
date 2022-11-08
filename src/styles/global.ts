63949c067c4706194ee8e6df1cca6a0ad084142c
KevinGuedes <kevinguedes@edu.unifor.br>
2022-11-08 19:12:20 -0300

    feat(pages): main app routes created

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	:focus {
+		outline: 0;
+		box-shadow: 0 0 0 1px ${(props) => props.theme.colors.brand.blue};
+	}
+
+  body {
+    background-color: ${(props) => props.theme.colors.base.background};
+    -webkit-font-smoothing: antialiased;
+  }
+
+	body, input, textarea, button {
+		font: 400 ${(props) => props.theme.sizes.m} 'Nunito', sans-serif;
+    line-height: 1.6;
+  }
+`
